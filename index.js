e55e21f380994292ef8e036d362531f6e3f08823
BitBD <tanvirtex@gmail.com>
2020-02-09 18:08:09 +0600

    ndex node client

---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * Proof of Stake Cryptocurrency generator
+ * Author: Sandoche ADITTANE
+ */
+
+'use strict';
+
+const inquirer = require('inquirer');
+const cmd = require('node-cmd');
+const Promise = require('bluebird');
+const replace = require('replace-in-file');
+
+
+console.log('*************************************************************');
+console.log('Welcome to nDEX Network');
+console.log('*************************************************************');
+console.log(' ')
+
+
